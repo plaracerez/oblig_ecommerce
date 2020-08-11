7e086552a18184638fd3fa37259180ac7a1d7f63
plaracerez <claraperezpucci@hotmail.com>
2020-08-11 11:19:54 -0300

    Entregable del 16 partes 1 y 2 (incompleto)

--- js/products.js
+++ js/products.js
@@ -4,3 +4,38 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+
+
+
+
+var productsArray = [];
+
+function showProducts(array) {
+
+    let contenido = "";
+    for (let i = 0; i < array.length; i++) {
+        let products = array[i];
+
+        contenido += products.name + '<br>';
+        contenido += products.description + '<br>';
+        contenido += 'Costo: ' + products.cost + '<br>';
+        contenido += 'Moneda: ' + products.currency + '<br>';
+        contenido += 'Imagen: ' + products.imgSrc + '<br>';
+        contenido += 'Cantidad de ventas: ' + products.soldcount + '<br>';
+
+        contenido += '<br><hr><br>'
+
+
+        document.getElementById("listado").innerHTML = contenido;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+
+            showProducts(productsArray);
+        }
+    })
+})
