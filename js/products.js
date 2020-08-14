5f2ca99ab54292691fdd87261bb3ce54337fc152
plaracerez <claraperezpucci@hotmail.com>
2020-08-14 09:11:26 -0300

    Hice estilo de products.html

--- js/products.js
+++ js/products.js
@@ -6,25 +6,41 @@ document.addEventListener("DOMContentLoaded", function (e) {
 });
 
 
-
+//en esta parte inserto los elementos en el html con sus estilos correspondientes
 var productsArray = [];
 
 function showProducts(array) {
 
     let contenido = "";
-    for (let i = 0; i < array.length; i++) {
-        let products = array[i];
 
-        contenido += products.name + '<br>';
-        contenido += products.description + '<br>';
-        contenido += 'Precio contado: ' + products.currency + " " + products.cost + '<br>';
-        contenido += "<img src=" + products.imgSrc + "> <br>";
-        contenido += '<br><hr><br>'
+for (i = 0; i < array.length; i++) {
+    let products = array[i];
+    contenido += `
+    <a href="product-info.html" class="list-group-item list-group-item-action">
+    <div class="row">
 
-        document.getElementById("listado").innerHTML = contenido;
-    }
+        <div class="col-3">
+            <img src="`+ products.imgSrc +`"class="img-thumbnail">
+        </div>
+
+        <div class="col">
+            <div class="d-flex w-100 justify-content-between">
+                <h2 class="mb-1">`+products.name+`</h2>
+                <p class="text-muted">Vendidos: `+products.soldCount+`</p>
+            </div>
+            <div>
+                <p class="mb-1 text-muted">`+products.description+`</p>
+                <p class="mb-1 text-muted">`+products.currency+" "+products.cost+`</p>
+            </div>
+        </div>
+
+    </div>
+    </a>
+`
+document.getElementById("listado").innerHTML = contenido;
+
 }
-
+}
 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function (resultObj) {
