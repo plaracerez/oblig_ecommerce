949c0c59dae4f4857bbdd15faf37415db72608ee
plaracerez <claraperezpucci@hotmail.com>
2020-08-24 12:02:36 -0300

    Hice el filtro

--- js/products.js
+++ js/products.js
@@ -13,33 +13,33 @@ function showProducts(array) {
 
     let contenido = "";
 
-for (i = 0; i < array.length; i++) {
-    let products = array[i];
-    contenido += `
+    for (i = 0; i < array.length; i++) {
+        let products = array[i];
+        contenido += `
     <a href="product-info.html" class="list-group-item list-group-item-action">
     <div class="row">
 
         <div class="col-3">
-            <img src="`+ products.imgSrc +`"class="img-thumbnail">
+            <img src="`+ products.imgSrc + `"class="img-thumbnail">
         </div>
 
         <div class="col">
             <div class="d-flex w-100 justify-content-between">
-                <h2 class="mb-1">`+products.name+`</h2>
-                <p class="text-muted">Vendidos: `+products.soldCount+`</p>
+                <h2 class="mb-1">`+ products.name + `</h2>
+                <p class="text-muted">Vendidos: `+ products.soldCount + `</p>
             </div>
             <div>
-                <p class="mb-1 text-muted">`+products.description+`</p>
-                <p class="mb-1 text-muted">`+products.currency+" "+products.cost+`</p>
+                <p class="mb-1 text-muted">`+ products.description + `</p>
+                <p class="mb-1 text-muted">`+ products.currency + " " + products.cost + `</p>
             </div>
         </div>
 
     </div>
     </a>
 `
-document.getElementById("listado").innerHTML = contenido;
+        document.getElementById("listado").innerHTML = contenido;
 
-}
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function (e) {
@@ -50,4 +50,35 @@ document.addEventListener("DOMContentLoaded", function (e) {
             showProducts(productsArray);
         }
     })
-})
+});
+
+
+
+document.getElementById("filtroProductos").addEventListener("click", function () {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+
+        var valorMin = document.getElementById("filtroMin").value;
+        var valorMax = document.getElementById("filtroMax").value;
+
+        if (resultObj.status === "ok") {
+            let filtro = resultObj.data.filter(function (elemento) {
+                return elemento.cost >= valorMin && elemento.cost <= valorMax;
+            })
+
+            showProducts(filtro);
+        };
+    });
+});
+
+document.getElementById("limpiar").addEventListener("click", function () {
+
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+
+        document.getElementById("filtroMin").value = "";
+        document.getElementById("filtroMax").value = "";
+
+        if (resultObj.status === "ok") {
+            showProducts(resultObj.data);
+        }
+    });
+});
