f5fbe42341199cb4201148062339d6201b2166b2
plaracerez <claraperezpucci@hotmail.com>
2020-09-15 16:33:34 -0300

    Hice carrusel y productos relacionados

--- js/product-info.js
+++ js/product-info.js
@@ -3,6 +3,7 @@
 //elementos HTML presentes.
 var producto = {};
 var comentariosArray = [];
+var productsArray = [];
 
 function showProduct(auto, arrayComments) {
 
@@ -27,12 +28,49 @@ function showProduct(auto, arrayComments) {
                 `
 
     imgs += `
+    <div class="container" id="carrusel">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner img-thumbnail">
+                <div class="carousel-item active">
+                    <img src="${auto.images[1]}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="${auto.images[2]}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="${auto.images[3]}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="${auto.images[4]}" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <br>
+
+    <!--esto lo oculto porque es versión vieja
         <div class="container">
             <img class="m-2 img-thumbnail" src="${auto.images[1]}" width="500px" alt="">
             <img class="m-2 img-thumbnail" src="${auto.images[2]}" width="500px" alt="">
             <img class="m-2 img-thumbnail" src="${auto.images[3]}" width="500px" alt="">
             <img class="m-2 img-thumbnail" src="${auto.images[4]}" width="500px" alt="">
-        </div>`;
+        </div>
+    -->`;
+    
 
 
     arrayComments.forEach(function (comment) {
@@ -56,7 +94,7 @@ function showProduct(auto, arrayComments) {
 
     });
 
-    
+
 
 
 
@@ -64,6 +102,20 @@ function showProduct(auto, arrayComments) {
     document.getElementById("imagenes").innerHTML = imgs;
     document.getElementById("comentarios").innerHTML = comments;
 }
+
+
+//relacionados
+function showRelatedProducts(arrayListado, arrayRelacionados) {
+    let contenido = "";
+
+    arrayRelacionados.forEach(function (i) {
+        contenido += `<strong>${arrayListado[i].name}</strong><br>${arrayListado[i].description}<br>
+        <img src="${arrayListado[i].imgSrc}" width="100px"><br><br>`
+    });
+
+    document.getElementById("relatedProducts").innerHTML = contenido;
+}
+
 
 document.addEventListener("DOMContentLoaded", function (e) {
 
@@ -80,6 +132,16 @@ document.addEventListener("DOMContentLoaded", function (e) {
             showProduct(auto, comentariosArray);
         }
     });
+
+    //relacionados
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+
+            showRelatedProducts(productsArray, auto.relatedProducts);
+        }
+    })
+
 
     //comentar
     let userLogged = localStorage.getItem("user-logged");
